22c9672800f9ab716f9aa350da0904b49878008d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-11-24 00:27:24 +0530

    sm7325-common: Disable ART debug and optimize dexpreopt
     * don't include debug configs in art
     * use everything filter for dexpreopt (https://github.com/AOSPA/android_vendor_pa/commit/ab5061c425e61bc7d36f7bcf480feea975795545)
     * use non-debug dex2oat
    
    Change-Id: Ic310df1e2c466437b26fcb2e5ab9333694328c48
    
    Signed-off-by: Notkerd69 <jerespn@gmail.com>
    
    Original patch:
    https://github.com/aospa-rhodep/device_motorola_rhodep/commit/b4e742937fae6aecde57696a720a1a883f838f1f

--- common.mk
+++ common.mk
@@ -116,6 +116,11 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# Dex/ART optimization
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+USE_DEX2OAT_DEBUG := false
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
