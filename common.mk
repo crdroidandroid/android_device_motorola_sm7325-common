5fc27b060eb83a3492013f86c4ed0c70e850d99d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-03-26 16:50:31 +0530

    sm7325-common: Import MSM IRQ balancer configuration

--- common.mk
+++ common.mk
@@ -191,6 +191,10 @@ PRODUCT_COPY_FILES += \
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := false
 
+# IRQ balancer config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
