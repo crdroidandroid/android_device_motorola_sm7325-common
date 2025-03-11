c524a7d660fff00f48441821cbdfd0c0cdbf9573
Michael Bestas <mkbestas@lineageos.org>
2025-03-11 02:17:16 +0200

    sm7325-common: Add hardware/motorola to soong namespaces
    Change-Id: I1606d2dc9cef82a4ce9d8764c1f52b59d183e9b2

--- common.mk
+++ common.mk
@@ -321,6 +321,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/motorola \
     hardware/google/interfaces \
     hardware/google/pixel
 
